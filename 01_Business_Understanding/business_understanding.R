b6339f0456f431f416b607b3611f9426c7578010
Fengjia Chen <fc2691@cumc.columbia.edu>
2021-11-10 17:19:09 -0500

    Making Plotting Function.

--- 01_Business_Understanding/business_understanding.R
+++ 01_Business_Understanding/business_understanding.R
@@ -151,6 +151,72 @@ dept_job_role_tbl %>%
 
 # Visualization of Attrition Cost----
 
+plot_attrition <- function(data, ..., .value, 
+                           fct_reorder = TRUE, 
+                           fct_rev = FALSE, 
+                           include_lbl = TRUE, 
+                           color = palette_light()[[1]], 
+                           units = c("0", "K", "M")) {
+  # Inputs
+  group_vars_expr <- quos(...)# capture the column name without evaluating, just store them. 
+  if (length(group_vars_expr) == 0)
+    group_vars_expr <- quos(rlang::sym(colnames(data)[[1]]))
+  
+  value_expr <- enquo(.value)
+  value_name <- quo_name(value_expr)
+  
+  units_val <- switch(units[[1]],
+                      "M" = 1e6,
+                      "K" = 1e3,
+                      "0"  = 1)
+  if (units[[1]] == "0") 
+    units <- ""
+  
+  # Data Manipulation
+  
+ usd <- scales::dollar_format(prefix = "$")
+    
+  data_manipulated <- 
+    data %>% 
+    mutate(name = str_c(!!!group_vars_expr, sep = ": ") %>% as_factor()) %>% 
+    # pull(name) %>% 
+    # levels()
+    mutate(value_text = str_c(usd(!! value_expr / units_val), 
+                            units[[1]], sep = "")) 
+    
+    if (fct_reorder) {
+      data_manipulated <- 
+        data_manipulated %>% 
+        mutate(name = forcats::fct_reorder(name, !! value_expr)) %>% 
+        arrange(name)
+    }
+  
+    if (fct_rev) {
+      data_manipulated <- 
+        data_manipulated %>% 
+        mutate(name = forcats::fct_rev(name)) %>% 
+        arrange(name)
+    }
+  
+  # Visualization
+  g <- data_manipulated %>% 
+    ggplot(aes_string(x = value_name, y = "name")) +
+    geom_segment(aes(xend = 0, yend = name), color = color) + 
+    geom_point(aes_string(size = value_name), color = color) + 
+    scale_x_continuous(labels = scales::dollar) + 
+    theme_tq() + 
+    scale_size(range = c(3, 5)) + 
+    theme(legend.position = "none")
+  
+  if (include_lbl) {
+    g <-
+      g + 
+        geom_label(aes_string(label = "value_text", size = value_name), 
+               hjust = "inward", color = color)
+  }
+  return(g)
+}
+
 dept_job_role_tbl %>% 
   count(Department, JobRole, Attrition) %>% 
   count_to_pct(Department, JobRole) %>% 
@@ -158,24 +224,11 @@ dept_job_role_tbl %>%
   mutate(
     cost_of_attrition = calculate_attrition_cost(n = n, salary = 80000)
   ) %>% 
+  plot_attrition(Department, JobRole, .value = cost_of_attrition, units = "M") + 
+  labs(
+    title = "Estimated Cost of Attrition by Job Roles", 
+    x = "Cost of Attrition", 
+    subtitle = "Looks like sales executive and laboratory technician are the biggest drivers of cost"
+  )
 
-# Data Manipulation
-  mutate(name = str_c(Department, JobRole, sep = ": ") %>% as_factor()) %>% 
-  # pull(name) %>% 
-  # levels()
-  mutate(name = fct_reorder(name, cost_of_attrition)) %>% 
-  mutate(cost_text = str_c("$", format(cost_of_attrition / 1e6, digits = 2), 
-                           "M", sep = "")) %>% 
-  # Plotting
-  ggplot(aes(x = cost_of_attrition, y = name)) +
-  geom_segment(aes(xend = 0, yend = name), color = palette_light()[[1]]) + 
-  geom_point(aes(size = cost_of_attrition), color = palette_light()[[1]]) + 
-  scale_x_continuous(labels = scales::dollar) + 
-  geom_label(aes(label = cost_text, size = cost_of_attrition), 
-             hjust = "inward", color = palette_light()[[1]]) + 
-  theme_tq() + 
-  scale_size(range = c(3, 5)) + 
-  labs(title = "Estimated Cost of Attrition: By Dept and Job Role", 
-       y = "", x = "Cost of Attrition") + 
-  theme(legend.position = "none")
 
